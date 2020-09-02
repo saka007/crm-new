b1fc813923829ed8dd07327199fb125f818d00cd
abdul <samunleash1@yahoo.com>
2020-09-03 01:54:47 +0400

    converged

--- head.php
+++ head.php
@@ -47,6 +47,9 @@ $ces1=$ces->fetch_array();
 
   <title>Client - Dashboard</title>
 
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap-datepicker.css">
+
   <!-- Custom fonts for this template-->
   <link rel="stylesheet" href="theme/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
@@ -67,6 +70,16 @@ $ces1=$ces->fetch_array();
   <link rel="stylesheet" href="theme/plugins/summernote/summernote-bs4.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <link href="vendor/datatables/css/dataTables.bootstrap4.css" rel="stylesheet">
+
+<link href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script src="theme/package/dist/sweetalert2.all.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.0/dropzone.min.js"></script>
+
+<link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" rel="stylesheet">
+
 </head>
 
 <body id="page-top">
