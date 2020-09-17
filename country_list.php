11ba55cdc0f160d7e299c67ca888a40e4c6258ae
Firstname Lastname <saquibrahmani10182@gmail.com>
2020-09-18 03:47:31 +0530

    Designing and lead addition correction

--- country_list.php
+++ country_list.php
@@ -1,16 +1,40 @@
 <?php
 include_once("head.php");	
 ?>
-    <!-- Begin Page Content -->
-    <div class="content-wrapper">
+<!-- Use only where datatable is required -->
+<!-- DataTables -->
+<link rel="stylesheet" href="theme/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="theme/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">	
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Country Management</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                        <li class="breadcrumb-item active">Country Management</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <section class="content">
+		<div class="container-fluid">
 			<div class="row">
-             <div class="col-sm-10">
-                    <h4 class="mb-3">Country Management <a href="javascript:void(0);" class="btn btn-info pull-right" data-toggle="modal" data-target="#newForm"  style="float:right;">Add New <i class="fa fa-plus"></i></a></h4>
-               
-                            <table class="table table-striped table-bordered" id="dataTables-Table" style="width:100%">
+				<div class="col-12">
+					<!-- /.card -->
+					<div class="card">
+						<div class="card-header">
+                        <h4 class="mb-3" style="float:right;"> <a href="javascript:void(0);" class="btn btn-primary pull-right" data-toggle="modal" data-target="#newForm">Add New <i class="fa fa-plus"></i></a></h4>
+						</div>
+						<!-- /.card-header -->
+						<div class="card-body">
+                             <table class="table table-striped table-bordered display nowrap" id="mydataTable">
                                 <thead>
 									<tr>
 									  <th>Country Name</th>
@@ -27,11 +51,11 @@ include_once("head.php");
 								<tr id="item-<?=$res2['id']?>">
 								 <td><?=$res2['name'];?></td>
 	
-									 <td style="text-align:right" >
+                                 <td>
+								  <p style="text-align:right">
 									 <a href="javascript:void(0);" data-toggle="modal" data-target="#editForm<?=$res2['id']?>" class="btnEditAction"><i class="fa fa-edit" title="EDIT"></i></a>
 									<a href="javascript:void(0);" id="<?=$res2['id'];?>" class="btnDeleteAction"><i class="fa fa-trash" title="DELETE"></i></a> 
-								 </td>   
-								</tr>
+								  </p>
 								
 <div class="modal fade" id="editForm<?=$res2['id']?>" tabindex="-1" role="dialog" >
     <div class="modal-dialog">
@@ -66,60 +90,95 @@ include_once("head.php");
 </div>
 
 <script>
-$(document).ready(function() {
-    $('#popForm<?=$res2['id']?>').formValidation({
-        framework: 'bootstrap',
-        excluded: ':disabled',
-        icon: {
-            valid: 'fa fa-ok',
-            invalid: 'fa fa-remove',
-            validating: 'fa fa-refresh'
-        },
-        fields: {
-            name: {
-                validators: {
-                    notEmpty: {
-                        message: 'The name is required'
-                    }
-                }
-            }
-        }
-    })
-	.on('success.form.fv', function(e) {
-            // Prevent form submission
-            e.preventDefault();
-            // Use Ajax to submit form data
-            $.ajax({
-                url: 'process/country_process.php',
-                type: 'POST',
-				dataType: 'json',
-                data: $('#popForm<?=$res2['id']?>').serialize(),
-                success: function(result) { 
-					if(result.status=='success')
-					{
-					$('#alert-success<?=$res2['id']?>').css('display','block');
-					setTimeout(function(){ $('#alert-success<?=$res2['id'];?>').css('display','none');},2000);
-					setTimeout(function(){ location.reload();},2000);
-					}
-					else
-					{
-					$('#alert-danger<?=$res2['id']?>').css('display','block');
-					setTimeout(function(){ $('#alert-danger<?=$res2['id'];?>').css('display','none');},3000);
-					}
-               }
-            });
-        });
+					$(document).ready(function() {
+						$('#popForm<?=$res2['id']?>').validate({
+								rules: {
+									name: {
+										required: true,
+									},
+								},
+								messages: {
+									name: "Country name is required",
+								},
+                            errorElement: 'span',
+                            errorPlacement: function(error, element) {
+                                error.addClass('invalid-feedback');
+                                element.closest('.form-group').append(error);
+                            },
+                            highlight: function(element, errorClass, validClass) {
+                                $(element).addClass('is-invalid');
+                            },
+                            unhighlight: function(element, errorClass, validClass) {
+                                $(element).removeClass('is-invalid');
+                            },
+                            success: function(label, element) {
+                                if ($(element).hasClass("is-invalid")) {
+                                    $(element).addClass("is-valid");
+                                }
+                            },
+                            submitHandler: function() {
+                                var formData = new FormData($('#popForm<?= $res2['id'] ?>')[0]);
+                                $.ajax({
+                                    url: 'process/country_process.php',
+                                    type: 'POST',
+                                    enctype: 'multipart/form-data',
+                                    dataType: 'json',
+                                    data: formData,
+                                    processData: false,
+                                    contentType: false,
+                                    cache: false,
+                                    success: function(result) {
+                                        if (result.status == 'success') {
+                                            $('#alert-success<?= $res2['id'] ?>').css('display', 'block');
+                                            setTimeout(function() {
+                                                $('#alert-success<?= $res2['id']; ?>').css('display', 'none');
+                                            }, 2000);
+                                            setTimeout(function() {
+                                                location.reload();
+                                            }, 2000);
+                                        } else {
+                                            $('#alert-danger<?= $res2['id'] ?>').css('display', 'block');
+                                            setTimeout(function() {
+                                                $('#alert-danger<?= $res2['id']; ?>').css('display', 'none');
+                                            }, 3000);
+                                        }
+                                    },
+                                    error: function(error) {
+                                        console.log(error);
+                                    }
+                                });
+                            }
+                        });
+                        });
 
-});
+                        </script>
+		 					</td>
+										</tr>
+									<?php $i++;
+									} ?>
 
-</script>											
-		<?php $i++;} ?>
-                                </tbody>
-                            </table>
-                            <!-- /.table-responsive -->
-                </div>
-                <!-- /.col-lg-12 -->
-           
+								</tbody>
+
+							</table>
+
+							<!-- /.table-responsive -->
+
+							<!-- </div> -->
+
+							<!-- /.col-lg-12 -->
+						</div>
+						<!-- /.card-body -->
+					</div>
+					<!-- /.card -->
+				</div>
+				<!-- /.col -->
+			</div>
+			<!-- /.row -->
+		</div>
+		<!-- /.container-fluid -->
+	</section>
+	<!-- /.content -->
+</div>
 <div class="modal" id="newForm">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -150,52 +209,88 @@ $(document).ready(function() {
         </div>
     </div>
 </div>
-<?php 	include_once("foot.php");	?>
-	<script src="js/formvalidation.js"></script>
+<?php include_once("foot.php");	?>
 
+<script src="theme/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="theme/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="theme/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="theme/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 <script>
-
 $(document).ready(function() {
-    $('#popForm').formValidation({
-        framework: 'bootstrap',
-        excluded: ':disabled',
-        icon: {
-            valid: 'fa fa-ok',
-            invalid: 'fa fa-remove',
-            validating: 'fa fa-refresh'
-        },
-        fields: {
-            name: {
-                validators: {
-                    notEmpty: {
-                        message: 'The name is required'
-                    }
-                }
-            }
-        }
-    })
-	.on('success.form.fv', function(e) {
-           e.preventDefault();
-            $.ajax({
-                url: 'process/country_process.php',
-                type: 'POST',
-				dataType: 'json',
-                data: $('#popForm').serialize(),
-                success: function(result) {
-					if(result.status=='success')
-					{
-					$('.alert-success').css('display','block');
-					setTimeout(function(){ $('.alert-success').css('display','none');},1000);
-					setTimeout(function(){ location.reload();},1000);
-					}
-					else
-					{
-					$('.alert-danger').css('display','block');
-					setTimeout(function(){ $('.alert-danger').css('display','none');},1000);
-					}
-               }
-            });
+
+	$("#mydataTable").DataTable({
+			 "responsive": true,
+			 "autoWidth": true,
+			// "scrollY": true,
+			//"scrollX": true,
+			// dom: 'Bfprt',
+			buttons: [{
+				extend: 'excel',
+				title: 'Source Report',
+				messageTop: 'Source Data'
+			}]
+
         });
+
+    $('#popForm').validate({
+			rules: {
+				name: {
+					required: true,
+				},
+
+			},
+			messages: {
+				name: "Country name is required",
+			},
+			errorElement: 'span',
+			errorPlacement: function(error, element) {
+				error.addClass('invalid-feedback');
+				element.closest('.form-group').append(error);
+			},
+			highlight: function(element, errorClass, validClass) {
+				$(element).addClass('is-invalid');
+			},
+			unhighlight: function(element, errorClass, validClass) {
+				$(element).removeClass('is-invalid');
+			},
+			success: function(label, element) {
+				if ($(element).hasClass("is-invalid")) {
+					$(element).addClass("is-valid");
+				}
+			},
+			submitHandler: function() {
+				var formData = new FormData($('#popForm')[0]);
+				$.ajax({
+					url: 'process/country_process.php',
+					type: 'POST',
+					enctype: 'multipart/form-data',
+					dataType: 'json',
+					data: formData,
+					processData: false,
+					contentType: false,
+					cache: false,
+					success: function(result) {
+						if (result.status == 'success') {
+							$('.alert-success').css('display', 'block');
+							setTimeout(function() {
+								$('.alert-success').css('display', 'none');
+							}, 1000);
+							setTimeout(function() {
+								location.reload();
+							}, 1000);
+						} else {
+							$('.alert-danger').css('display', 'block');
+							setTimeout(function() {
+								$('.alert-danger').css('display', 'none');
+							}, 1000);
+						}
+					},
+					error: function(error) {
+						console.log(error);
+					}
+				});
+			}
+	});
 
 	$('body').on('click','.btnDeleteAction',function(){ 
 				 var tr = $(this).closest('tr');
